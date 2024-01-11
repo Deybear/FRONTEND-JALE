30b5087d0bf78454963432a02a171604b1c46b91
Deybear <drodriguezr@fwdcostarica.com>
2024-01-11 15:41:45 -0600

    nineth commit

--- jale-frontend/src/pages/Tours.jsx
+++ jale-frontend/src/pages/Tours.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Grid.css';
 import '../styles/Tours.css';
 import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
 import Navigation from '../components/Navigation';
+import Places from '../services/Places';
 
 function Tours()
 {
+    const [place, setPlace] = useState([]);
+    const service = new Places();
+
+    useEffect(() => {
+        getPlace();
+    }, [])
+
+    const getPlace = async () => {
+        const data = await service.getPlaces();
+        setPlace(data);
+    }
+
+    console.log(place);
+
     return (
 
         <section className='main-section'>
@@ -16,6 +31,27 @@ function Tours()
 
                 {/* - - - </> [NAV] </> - - - */}
                 <Navigation/>
+
+                {/* - - - </> [DIV] </> - - - */}
+                <div className='places-list'>
+                    
+                    {/* - - - </> [PLACE] </> - - - */}
+                    {place.map((item, index) => (
+
+                        //* - - - </> [DIV] </> - - - *//
+                        <div className='place-card' key={index}>
+
+                            {/* - - - </> [TEXT] </> - - - */}
+                            <p className='place-name'>{item.place_name}</p>
+
+                            {/* - - - </> [TEXT] </> - - - */}
+                            <p className='place-address'>{item.place_address}</p>
+
+                        </div>
+
+                    ))}
+                    
+                </div>
 
             </div>
 
---
+++ jale-frontend/src/services/Places.js
@@ -0,0 +1,92 @@
+import axios from "axios";
+
+export default class Places
+{
+    // - - - </> [GET] </> - - - //
+    async getPlaces()
+    {
+        try
+        {
+            // - - - </> [URL] </> - - - //
+            const { data } = (await axios.get('http://localhost:3000/api/places'));
+            
+            return data;
+        }
+        catch (error)
+        {
+            // - - - </> [ERROR] </> - - - //
+            console.log(error);
+            throw error;
+        }
+    }
+
+    // - - - </> [POST] </> - - - //
+    async createPlace(data)
+    {
+        try
+        {
+            //* - - - </> [URL] </> - - - *//
+            const response = await axios.post('http://localhost:3000/api/places', data);
+            console.log(response);
+            return response.data;
+        }
+        catch(error)
+        {
+            // - - - </> [ERROR] </> - - - //
+            console.log(error);
+            throw error;
+        }
+    }
+
+    // - - - </> [GET] </> - - - //
+    async getPlace(id)
+    {
+        try
+        {
+            // - - - </> [URL] </> - - - //
+            const { data } = await axios.get(`http://localhost:3000/api/places/${id}`);
+            return data;
+        }
+        catch (error)
+        {
+            // - - - </> [ERROR] </> - - - //
+            console.log(error);
+            throw error;
+        }
+    }
+
+    // - - - </> [PUT] </> - - - //
+    async updatePlace(id, data)
+    {
+        try
+        {
+            // - - - </> [URL] </> - - - //
+            await axios.put(`http://localhost:3000/api/places/${id}`, data);
+            return {status: 200, message: 'Content updated successfully!'};
+        }
+        catch(error)
+        {
+            // - - - </> [ERROR] </> - - - //
+            console.log(error);
+            throw error;
+        }
+    }
+
+    // - - - </> [DELETE] </> - - - //
+    async deletePlace(id)
+    {
+        try
+        {
+            // - - - </> [URL] </> - - - //
+            await axios.delete(`http://localhost:3000/api/places/${id}`);
+            return {status: 200, message: 'Content removed successfully!'};
+        }
+        catch(error)
+        {
+            // - - - </> [ERROR] </> - - - //
+            console.log(error);
+            throw error;
+        }
+    }
+
+};
